aa8dd522a70fb524f3d0d5b6b7799667c46d2d30
NxPKG <iconmamundentist@gmail.com>
2024-11-30 04:28:19 +0600

    Create trailing-spaces.js
    Signed-off-by: NxPKG <iconmamundentist@gmail.com>

---
+++ fixers/trailing-spaces.js
@@ -0,0 +1,10 @@
+const os = require('os');
+
+exports.register = async (fixers) => {
+  fixers[0].push({
+    id: 'trailing-spaces',
+    // Fix only non-binary files. Source: https://unix.stackexchange.com/a/36240
+    cmd: `git grep -I --name-only -z -e '' | xargs -0 sed ${os.type() === 'Darwin' ? '-i "" -E' : '-i -e'} "s/[[:space:]]*$//"`,
+    description: 'Fix trailing spaces',
+  });
+};
