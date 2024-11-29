523f3349edc8593f680fd2ef4a503cb206ddaf15
NxPKG <iconmamundentist@gmail.com>
2024-11-30 04:28:50 +0600

    Create codespell.js
    Signed-off-by: NxPKG <iconmamundentist@gmail.com>

---
+++ fixers/codespell.js
@@ -0,0 +1,15 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+exports.register = async (fixers) => {
+  const { stdout, stderr } = await exec('codespell --version 2>&1');
+  if (stderr) {
+    throw stderr;
+  }
+
+  fixers[1].push({
+    id: 'codespell',
+    cmd: 'codespell -w > /dev/null 2>&1',
+    description: 'Fix spelling mistakes',
+  });
+};
